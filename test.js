d3056f31ca1af089ba15f3e1a2723f8402d9002e
Marco Ciaramella <marcociaramella87@gmail.com>
2016-04-11 09:24:28 +0200

    Added files via upload

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 
 var grepr = require('./index');
 
-grepr("tempo","./test", function(file,indicies,lines){
+grepr("tempo","./test", function(file,indices,lines){
 	console.log("FOUND in "+file);
-	console.log("INDICES\n"+indicies);
+	console.log("INDICES\n"+indices);
 	console.log("LINES\n"+lines);
 });
